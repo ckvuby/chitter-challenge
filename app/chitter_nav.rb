e87a10a2d4377ac5e8cf57c33bb62ffb61a20507
Vu Le <core.safecrlf>
2018-09-28 15:36:22 +0100

    implement simplest code to pass feature test

--- app/chitter_nav.rb
+++ app/chitter_nav.rb
@@ -4,4 +4,10 @@ class ChitterNav < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  post '/message_board' do
+    @name = params[:firstname]
+    @peep = params[:msg]
+    erb :message_board
+  end
 end
--- spec/feature/post_msg_spec.rb
+++ spec/feature/post_msg_spec.rb
@@ -6,11 +6,9 @@ feature 'So that I can update people on the internet' do
   scenario 'User can post a message/peep to chitter' do
     visit('/')
     fill_in('firstname', with: 'Vu')
-    fill_in('surname', with: 'Le')
-    fill_in('message', with: 'Hello World')
+    fill_in('msg', with: 'Hello World')
     click_button('Submit')
     expect(page).to have_content('Hello World')
     expect(page).to have_content('Vu')
-    expect(page).to have_content('Le')
   end
 end
